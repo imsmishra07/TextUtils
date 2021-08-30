581c9fd4c99a2326acb9e8711f477fd37b6facc7
Sachin Mishra <mishrasachin0705@gmail.com>
2021-08-30 11:05:47 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import './App.css';
+import About from './Components/About';
+import Navbar from './Components/Navbar.js'
+import TextForm from './Components/TextForm.js'
+import React ,{useState}from 'react'
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
+  const [mode,setMode]=useState("light");
+  const [alert,setAlert]=useState(null);
+  const showAlert=(type,message)=>{
+
+    setAlert({
+        message:message,
+        type:type
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+      
+    }, 1500);
+
+  }
+  const toggleMode=()=>{
+
+    if(mode==="light")
+    {
+      setMode("dark");
+      document.body.style.backgroundColor="#265948"
+      showAlert("success","Darkmode Enabled")
+      setInterval(() => {
+        document.title="Text Utils"
+      }, 2000);
+      setInterval(() => {
+        document.title="Best Text Utility App"
+      }, 1500);
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white"
+      showAlert("success","Light Mode Enabled")
+
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+    <Router>
+    <div>
+     {/*<Navbar title="Sachin" about="About me"/>*/}
+      <Navbar mode={mode}   toggleMode={toggleMode}></Navbar>
+      
+      <div className="container my-3">
+      <Alert alert={alert}/>
+      <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm mode={mode} showAlert={showAlert} heading="Enter your text to analyse"  />
+          </Route>
+        </Switch>
+     
+      {/*<About></About>*/
+      }
+      </div>
+     
     </div>
+    </Router>
   );
 }
 
